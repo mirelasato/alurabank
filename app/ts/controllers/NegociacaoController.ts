16d9937a30fa982c705a80e54545b7913da6e59d
mirelasato <39499416+mirelasato@users.noreply.github.com>
2019-11-29 18:13:27 -0300

    Add files via upload

---
+++ app/ts/controllers/NegociacaoController.ts
@@ -0,0 +1,68 @@
+import { NegociacoesView, MensagemView } from '../views/index';
+import { Negociacao, Negociacoes } from '../models/index';
+import { domInject } from '../helpers/decorators/index'
+
+export class NegociacaoController {
+
+    @domInject('#data')
+    private _inputData: JQuery;
+    @domInject('#quantidade')
+    private _inputQuantidade: JQuery;
+    @domInject('#valor')
+    private _inputValor: JQuery;
+    private _negociacoes = new Negociacoes();
+    private _negociacoesView = new NegociacoesView('#negociacoesView');
+    private _mensagemView = new MensagemView('#mensagemView');
+    
+    constructor() {
+        // this._inputData = $('#data');
+        // this._inputQuantidade = $('#quantidade');
+        // this._inputValor = $('#valor');
+        this._negociacoesView.update(this._negociacoes);
+    }
+
+    adiciona(event: Event) {
+
+        event.preventDefault();
+
+        let data = new Date(this._inputData.val().replace(/-/g, ','));
+
+        if(!this._ehDiaUtil(data)) {
+
+            this._mensagemView.update('Somente negociações em dias úteis, por favor!');
+            return 
+        }
+
+        const negociacao = new Negociacao(
+            data, 
+            parseInt(this._inputQuantidade.val()),
+            parseFloat(this._inputValor.val())
+        );
+
+        this._negociacoes.adiciona(negociacao);
+
+        this._negociacoesView.update(this._negociacoes);
+        this._mensagemView.update('Negociação adicionada com sucesso!');
+    }
+
+    private _ehDiaUtil(data: Date) {
+
+        return data.getDay() != DiaDaSemana.Sabado && data.getDay() != DiaDaSemana.Domingo;
+    }
+
+    importarDados()
+    {
+        alert('oi');
+    }
+}
+
+enum DiaDaSemana {
+
+    Domingo, 
+    Segunda, 
+    Terca, 
+    Quarta, 
+    Quinta, 
+    Sexta, 
+    Sabado
+}
---
+++ app/ts/helpers/decorators/index.ts
@@ -0,0 +1,2 @@
+export * from './LogarTempoDeExecução';
+export * from './domInject';
